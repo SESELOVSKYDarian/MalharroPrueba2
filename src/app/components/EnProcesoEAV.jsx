a9c0cf536ad6ff50eee0b113ceb9839e2b72bc8d
Sara Willers <sarawillerslohr08@gmail.com>
2025-10-23 13:25:28 -0300

    link mejorado

--- src/app/components/EnProcesoEAV.jsx
+++ src/app/components/EnProcesoEAV.jsx
@@ -19,7 +19,10 @@ export default function EnProcesoEAV() {
       <div className="container-fluid">
         <div className="row">
           <div className="col-12">
-            <a href="../web 05 09/index.html" className="btn btn-volver">
+            <a
+              href="https://www.avmartinmalharro.edu.ar/inicio.html"
+              className="btn btn-volver"
+            >
               <img
                 src={asset("/malharrooficial/images/Icon_Volver.svg")}
                 alt="Volver al menú principal"
@@ -43,9 +46,10 @@ export default function EnProcesoEAV() {
           </h1>
           <div className="espaciado-vertical margen" />
           <p className="p1-r" style={{ color: "var(--color-neutral-6)" }}>
-            * La construcci&oacute;n de la web de la Malharro es un proyecto conjunto
-            que continuar&aacute; el pr&oacute;ximo a&ntilde;o de la mano de 4&deg; de Dise&ntilde;o
-            Gr&aacute;fico y estudiantes de la T&eacute;cnica N&deg;5. *
+            * La construcci&oacute;n de la web de la Malharro es un proyecto
+            conjunto que continuar&aacute; el pr&oacute;ximo a&ntilde;o de la
+            mano de 4&deg; de Dise&ntilde;o Gr&aacute;fico y estudiantes de la
+            T&eacute;cnica N&deg;5. *
           </p>
           <div className="espaciado-vertical margen" />
           <img
